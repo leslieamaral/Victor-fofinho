c151841977560706f3e9d464523222dda906c7a2
leslieamaral <107128972+leslieamaral@users.noreply.github.com>
2024-10-16 18:01:54 -0300

    Update RedesFavoritasMundo.js

--- grafico/RedesFavoritasMundo.js
+++ grafico/RedesFavoritasMundo.js
@@ -1,45 +1,30 @@
-import { getCSS, criarGrafico, incluirTexto } from "./common.js"
-
-async function redesFavoritasMundo() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const redes = Object.keys(dados)
-    const valores = Object.values(dados)
-
-    const data = [
-        {
-            values: valores,
-            labels: redes,
-            type: 'pie',
-            textinfo: 'label+percent'
-        }
-    ]
-
-    const layout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        height: 700,
-        title: {
-            text: 'Redes sociais que os usuários mais gostam',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                family: getCSS('--font'),
-                size: 30
-            }
-        },
-        legend: {
-            font: {
-                color: getCSS('--primary-color'),
-                size: 16
-            }
-        }
-    }
-
-    criarGrafico(data, layout)
-
-    incluirTexto(`Embora o <span>Instagram</span> ocupe a quarta posição em termos de número total de usuários entre as redes sociais, destaca-se como a <span>preferida pelos usuários</span>. Supera até mesmo o <span>Facebook</span>, a plataforma com mais usuários, sendo a terceira opção mais apreciada pelos usuários. <br>Essa preferência evidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação com outras redes sociais`)
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
-redesFavoritasMundo()
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
+
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config)
+}
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
+}
+
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
